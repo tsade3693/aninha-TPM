9c58fb46626ae302af8353b48cf7fa0023bc63ed
tsade3693 <56261095+tsade3693@users.noreply.github.com>
2019-10-07 10:35:37 -0300

    Arquivos Principais
    Neste lote de envio constam os arquivos bases de um projeto.

---
+++ conexao/conexao.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+//Conexão on-line
+$hostname = "localhost";
+$database = "contatos";
+$username = "root";
+$password = "usbw";
+
+if($conecta = mysqli_connect($hostname, $username, $password, $database)){
+    // echo "Conectado...............<br>";
+}else{
+    echo "Erro: ".mysqli_connect_error();
+}
+?>
